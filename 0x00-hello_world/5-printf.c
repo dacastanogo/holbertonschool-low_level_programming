fc1f2e1951563cd63d69da19a541b9e9141f0349
dacastanogo <dcastanogomez@gmail.com>
2019-06-20 03:26:23 +0000

    Mandatory tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Prints a specified string using printf
+ *
+ *Description: This function prints the string "with proper grammar, but the
+ *outcome is a piece of art," using the printf function.
+ *Return: 0 if the function ran to completion
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
