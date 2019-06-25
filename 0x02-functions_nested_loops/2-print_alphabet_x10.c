1d6d88a4b0de42b213876e363b4561695d1f1c63
dacastanogo <dcastanogomez@gmail.com>
2019-06-25 22:24:07 +0000

    Correcting format in 2 task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 int y;
 int x;
 
-for (y = '0'; y <= 10; y++)
+for (y = '0'; y <= 9; y++)
 {
 for (x = 'a'; x <= 'z'; x++)
 {
 _putchar(x);
+}
 _putchar('\n');
 }
 y++;
-}
 }
