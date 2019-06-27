8f89a0922a48e6c4456fdc52f038b396f45d0048
dacastanogo <dcastanogomez@gmail.com>
2019-06-27 20:47:04 +0000

    Task 6 to 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of lines
+ * Result - void
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
