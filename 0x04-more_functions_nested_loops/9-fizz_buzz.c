f94a516e7ec12e3d503d25044ea8b45b5fb91757
dacastanogo <dcastanogomez@gmail.com>
2019-06-27 22:00:41 +0000

    Final 9 task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,16 +2,16 @@
 
 /**
  * main - entry point
- * Result - 0
+ * Return: 0
  */
 
-int main (void)
+int main(void)
 {
 int x;
 
 for (x = 1; x <= 100; x++)
 {
-if ((x % 3 == 0) && (x % 5 == 0))
+if ((x % 15) == 0)
 {
 printf("FizzBuzz");
 printf(" ");
@@ -23,7 +23,7 @@ printf(" ");
 }
 else if (x % 5 == 0)
 {
-  if (x == 100)
+if (x == 100)
 {
 printf("Buzz");
 }
