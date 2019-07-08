6c2dc52b6153e168c9d80f240fab5defdb7cc814
root <dacastanogo@github.com>
2019-07-08 17:07:46 +0000

    first tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - function that locates a character in a string.
+ * @s: pointer to a string
+ * @c: char to be located
+ * Return: address of s
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int i = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] = c)
+{
+
+
+
