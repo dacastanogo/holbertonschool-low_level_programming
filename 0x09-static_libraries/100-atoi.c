6f081d4334bf046a6b3d587cf6ee531fb4312339
dacastanogo <dcastanogomez@gmail.com>
2019-07-14 21:27:37 +0000

    Correcting 100-atoi function typo

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,18 @@
-int _atoi(char *s);
+#include "holberton.h"
+
+/**
+ * _atoi - Function that convert a string to an integer.
+ * @s: String
+ * Return: Integer
+ */
+
+int _atoi(char *s)
+{
+int a = 0;
+
+while (s[a] != '\0')
+{
+a++;
+}
+return (a);
+}
