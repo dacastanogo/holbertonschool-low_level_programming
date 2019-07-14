de2e3132d936ab76f0f1c866e84f684a8393ce2d
dacastanogo <dcastanogomez@gmail.com>
2019-07-14 22:00:58 +0000

    i changed all the format in the 4 task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,27 +3,55 @@
 #include <stdlib.h>
 
 /**
- * main - program that adds positive numbers.
- * @argc: the number of arguments
- * @argv: pointers to arguments
- * Return: Always 0.
+ * check_digit - checks if a string contains any non-digit characters
+ * @x: string to check
+ * Return: 1 if string contains non-digit characters, 0 otherwise
  */
+
+int check_digit(char *x)
+{
+char digit[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int i;
+int j;
+int check;
+
+for (i = 0; x[i] != '\0'; i++)
+{
+check = 0;
+for (j = 0; j < 11; j++)
+{
+if (x[i] == digit[j])
+check++;
+}
+if (check == 0)
+return (1);
+}
+return (0);
+}
+
+/**
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ * Return: 1 if any argument contains non-number symbols, 0 otherwise
+ */
+
 int main(int argc, char *argv[])
 {
+int i;
 int sum = 0;
-char *c;
 
-while (--argc)
+for (i = 1; i < argc; i++)
 {
-for (c = argv[argc]; *c; c++)
+if (check_digit(argv[i]) == 1)
 {
-if (*c < '0' || *c > '9')
-{
-return (printf("Error\n"), 1);
+printf("Error\n");
+return (1);
 }
-sum += atoi(argv[argc]);
+
+sum += atoi(argv[i]);
 }
-}
+
 printf("%d\n", sum);
 return (0);
 }
