bc09bf46a6f5d4dc0fcd328efb8e4afcd66a942e
dacastanogo <dcastanogomez@gmail.com>
2019-07-18 04:20:33 +0000

    Task 0 to 5

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - function that creates an array
+ * of chars, and initializes it with
+ * a specific char.
+ * @size: size of the array
+ * @c: char to initialize the array with
+ * Return: pointer to the array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+char *p;
+unsigned int i;
+
+if (size > 0)
+{
+p = malloc(size * sizeof(char));
+if (p == NULL)
+return (NULL);
+}
+else
+return (NULL);
+for (i = 0; i < size; i++)
+p[i] = c;
+return (p);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,19 @@
+    
+/**
+ *This header contains all the function prototypes for
+ *0x0B - Malloc and Free
+ */
+
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
