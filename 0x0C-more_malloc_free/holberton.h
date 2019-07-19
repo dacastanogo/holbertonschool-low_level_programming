c325b143a4a2cc0b0bb721ff71ed9b972b80a911
dacastanogo <dcastanogomez@gmail.com>
2019-07-19 03:43:24 +0000

    Correcting task 0 to 1

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *This header contains all the function prototypes for
+ *0x0C - More Malloc and Free
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
