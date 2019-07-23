913a63ee6b098a7f59ddff84536ef16b1cb0821c
dacastanogo <dcastanogomez@gmail.com>
2019-07-23 03:37:28 +0000

    correcting format and structure of code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,15 @@
  */
 void print_dog(struct dog *d)
 {
-if (d)
-{
-printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f\n", d->age ? d->age : "(nil)");
-printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-}
+if (d == NULL)
+return;
+if (d->name == NULL)
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
 }
