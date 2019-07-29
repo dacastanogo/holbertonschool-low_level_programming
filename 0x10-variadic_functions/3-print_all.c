90a9f4088c9e2bd349bca725cd6836bd247501a5
dacastanogo <dcastanogomez@gmail.com>
2019-07-29 04:34:42 +0000

    Correcting task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,51 +5,50 @@
 
 /**
  * print_all - function that prints anything.
- * @format: string with format instructions 
+ * @format: string with format instructions
  * Return: void
  */
+
 void print_all(const char * const format, ...)
 {
-va_list args;
-char *str;
-int i = 0
-int j = 0;
+  va_list args;
+  char *str;
+  int i = 0, j;
 
-while (format && format[i])
-{
-va_start(args, format);
-while (format[i])
-{
-
-j = 1;
-switch (format[i++])
-{
-case 'c':
-printf("%c", va_arg(args, int));
-break;
-case 'i':
-printf("%d", va_arg(args, int));
-break;
-case 'f':
-printf("%f", va_arg(args, double));
-break;
-case 's':
-str = va_arg(args, char*);
-if (str)
-}
-printf("%s", str);
-break;
-}
-printf("(nil)");
-break;
-default:
-j = 0;
-break;
-}
-if (format[i] && j)
-printf(", ");
-}
-va_end(args);
-}
-printf("\n");
+  while (format && format[i])
+    {
+      va_start(args, format);
+      while (format[i])
+	{
+	  j = 1;
+	  switch (format[i++])
+	    {
+	    case 'c':
+	      printf("%c", va_arg(args, int));
+	      break;
+	    case 'i':
+	      printf("%d", va_arg(args, int));
+	      break;
+	    case 'f':
+	      printf("%f", va_arg(args, double));
+	      break;
+	    case 's':
+	      str = va_arg(args, char*);
+	      if (str)
+		{
+		  printf("%s", str);
+		  break;
+		}
+	      printf("(nil)");
+	      break;
+	    default:
+	      j = 0;
+	      break;
+	    }
+	  if (format[i] && j)
+	    printf(", ");
+	}
+      va_end(args);
+    }
+  printf("\n");
 }
