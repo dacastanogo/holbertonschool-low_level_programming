695de52a3d4b873c4df76b3756847010e7789258
dacastanogo <dcastanogomez@gmail.com>
2019-08-02 03:04:28 +0000

    Correcting header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _lists_h_
+#define _lists_h_
 
 /**
  * lists.h - containing all the necessary prototype functions for
@@ -18,9 +18,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 
@@ -29,3 +29,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
+#endif
