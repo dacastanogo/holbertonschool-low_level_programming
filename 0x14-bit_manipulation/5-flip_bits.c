be9b07dd0458cd163efba0f75a83c830f81e0cc6
dacastanogo <dcastanogomez@gmail.com>
2019-08-09 04:43:32 +0000

    Task 4 to 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * flip_bits -function that returns the number of bits you would need to flip
+ * to get from one number to another.
+ * @n: number one to be flipped
+ * @m: number two to be flipped to
+ * Return: number of bits flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int new_num = n ^ m;
+	int index = (sizeof(unsigned long int) * 8) - 1;
+	int bits_flipped = 0;
+
+	while (index >= 0)
+	{
+		if ((new_num >> index) & 1)
+			bits_flipped++;
+		index--;
+	}
+	return (bits_flipped);
+}
