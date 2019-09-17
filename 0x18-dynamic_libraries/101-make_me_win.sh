385a0b0c76e79caa05930ec1e35ac0072a7cd063
dacastanogo <dcastanogomez@gmail.com>
2019-09-16 23:45:05 -0500

    Adding LD_PRELOAD

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/arthurdamm/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libwinner.so
+wget -P /tmp https://github.com/dacastanogo/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libwinner.so
 export LD_PRELOAD=/tmp/libwinner.so
