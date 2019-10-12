6e37ea799dfaf1d5e75402e5603bdfa2efe2ad48
dacastanogo <dcastanogomez@gmail.com>
2019-10-11 20:52:49 -0500

    Tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - function that creates a hash table.
- * @size: size of the array.
- * Return: a pointer to the newly created hash table.
+ * hash_table_create - creates a hash table
+ * @size: the size of the hash table
+ *
+ * Return: pointer to hash table type
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    if (!size)
-        return (NULL);
-    
-    ht = malloc(sizeof(hash_table_t));
-    if (!ht)
-        return (NULL);
-    
-    ht->size = size;
-    ht->array = calloc(size, sizeof(hash_node_t));
-        if (!ht->array)
-        {
-                free(ht);
-                return (NULL);
-        }
-        return (ht);
+	if (!size)
+		return (NULL);
+
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,9 @@
-#ifndef _HASHTABLES_
-#define _HASTTABLES_
+#ifndef _HASH_TABLES_H
+#define _HASH_TABLES_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -34,5 +35,10 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
