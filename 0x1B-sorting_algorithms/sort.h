977cb1a85692bea86f28b5c229a3e7c03cf43d78
dacastanogo <dcastanogomez@gmail.com>
2019-10-29 16:11:11 -0500

    Added protoype of partition function

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,5 +27,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
+void fast_sort(int *array, size_t size, int lower, int upper);
+int lomuto_partition(int *array, size_t size, int lower, int upper);
 
 #endif
